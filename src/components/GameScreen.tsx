3364a8304fa371c891276d1c937e358ccd07eace
racy19 <anna.raichlova@gmail.com>
2025-12-08 19:47:15 +0100

    merge

--- src/components/GameScreen.tsx
+++ src/components/GameScreen.tsx
@@ -1,16 +1,24 @@
 interface GameScreenProps {
-    backgroundUrl?: string;
-    children?: React.ReactNode;
+  backgroundUrl?: string;
+  children?: React.ReactNode;
 }
 
 export const GameScreen = ({ backgroundUrl, children }: GameScreenProps) => {
-    return (
-        <div className="screen">
-            {backgroundUrl
-                ? <img className="background-image" src={backgroundUrl} alt="Background" />
-                : null
-            }
-            <h1 className="text">Hra běží...</h1>
+  return (
+    <div className="screen">
+      {backgroundUrl
+        ? <img className="background-image" src={backgroundUrl} alt="Background" />
+        : null
+      }
+      <div className="element-layer">
+        <p className="screen-text-top">Hra běží..tton 1 ty jeden curaku vyprcanej hlavne mi rekni kde mam t.</p>
+        <div className="button-container">
+          <button>button 1 ty jeden curaku vyprcanej hlavne mi rekni kde mam to pivo</button>
+          <button>button 2</button>
+          <button>button 3</button>
+          <button>button 4den curaku vyprcanej hlavne mi rekni kde mam to pivo</button>
         </div>
-    );
+      </div>
+    </div>
+  );
 }
